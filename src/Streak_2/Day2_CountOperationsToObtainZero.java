ab6e1a9cacf540f12c55b82d593e8cf7257bde9c
RAJATSEMWAL1711 <rajatsemwal17@gmail.com>
2025-11-09 09:59:04 +0530

    day2 work

---
+++ src/Streak_2/Day2_CountOperationsToObtainZero.java
@@ -0,0 +1,19 @@
+//2169. Count Operations to Obtain Zero
+
+package Streak_2;
+
+class Solution2 {
+    public int countOperations(int num1, int num2) {
+        int count = 0;
+        while(num1!=0 && num2!=0){
+            if(num1 >= num2){
+                num1 = num1-num2;
+            }
+            else{
+                num2 = num2 - num1;
+            }
+            count++;
+        }
+        return count;
+    }
+}
