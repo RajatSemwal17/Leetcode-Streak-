fd169049b94764e8cacf5e32b3d3af8e9c7a8121
RAJATSEMWAL1711 <rajatsemwal17@gmail.com>
2025-11-24 19:37:05 +0530

    day17 work

---
+++ src/Streak_2/Day17_BinaryPrefixDivisibleBy5.java
@@ -0,0 +1,22 @@
+package Streak_2;
+
+//1018. Binary Prefix Divisible By 5
+
+import java.util.*;
+
+class Solution17{
+    public List<Boolean> prefixesDivBy5(int[] nums) {
+        List<Boolean> list = new ArrayList<>();
+        int value = 0;
+        for(int i=0;i<nums.length;i++){
+            value = (value * 2 + nums[i]) % 5;
+            if(value == 0){
+                list.add(true);
+            }
+            else{
+                list.add(false);
+            }
+        }
+        return list;
+    }
+}
